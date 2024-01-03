453761101e09237dbd3e35e666ffd79c05f6ad1d
henry226 <henry226@bu.edu>
2024-01-02 23:29:43 -0500

    fundamentals

---
+++ Client/app.js
@@ -0,0 +1,19 @@
+const fetchGreeting = async () => {
+  const res = await fetch("http://localhost:9000/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: "query { greeting }",
+    }),
+  });
+
+  const { data } = await res.json();
+
+  return data.greeting;
+};
+
+fetchGreeting().then((greeting) => {
+  document.getElementById("greeting").textContent = greeting;
+});
